cc0c3eba9c6c5d9a85294a4b0add907c8aba2420
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-06-09 16:11:22 +0100

    Setup test networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,53 +17,47 @@ require("./tasks/api-consumer")
 
 
 require('dotenv').config()
-
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
-const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 // optional
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key"
+
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
+            // If you want to do some forking, uncomment this
+            forking: {
+              url: process.env.MAINNET_RPC_URL
+            }
         },
         localhost: {
         },
         kovan: {
-            url: KOVAN_RPC_URL,
+            url: process.env.KOVAN_RPC_URL,
             // accounts: [PRIVATE_KEY],
             accounts: {
-                mnemonic: MNEMONIC,
+                mnemonic: process.env.MNEMONIC,
             },
             saveDeployments: true,
         },
         rinkeby: {
-            url: RINKEBY_RPC_URL,
+            url: process.env.RINKEBY_RPC_URL,
             // accounts: [PRIVATE_KEY],
             accounts: {
-                mnemonic: MNEMONIC,
+                mnemonic: process.env.MNEMONIC,
             },
             saveDeployments: true,
         },
         ganache: {
             url: 'http://localhost:8545',
             accounts: {
-                mnemonic: MNEMONIC,
+                mnemonic: process.env.MNEMONIC,
             }
         }
     },
     etherscan: {
         // Your API key for Etherscan
         // Obtain one at https://etherscan.io/
-        apiKey: ETHERSCAN_API_KEY
+        apiKey: process.env.ETHERSCAN_API_KEY
     },
     namedAccounts: {
         deployer: {
